5f50aaf5b2b786577fe9eb5c9cc47c2b00370370
uide2 <arunabraham9947@gmail.com>
2018-11-30 15:28:16 +0530

    css updation

--- js/my.js
+++ js/my.js
@@ -1,24 +1,12 @@
-var slideIndex = 1;
-showDivs(slideIndex);
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
+$(document).ready(function(){
+    $('.dropdown-submenu a.test').on("click", function(e){
+      $(this).next('ul').toggle();
+      e.stopPropagation();
+      e.preventDefault();
+    });
+  });
 
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = x.length }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[slideIndex - 1].style.display = "block";
-}
-
-function showDiv() {
-    document.getElementById('.dropdown-menu').style.display = "block";
-}
 
 
 $(window).load(function() {
